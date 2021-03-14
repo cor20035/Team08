afd4bbbfe836c8094f904f177295bca0a4c5bbbc
cor20035 <78001725+cor20035@users.noreply.github.com>
2021-03-14 14:55:00 -0600

    Erik i can't get the page to load

--- app/src/main/java/com/example/tapordie/MainActivity.java
+++ app/src/main/java/com/example/tapordie/MainActivity.java
@@ -34,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
         this.getWindowManager().getDefaultDisplay().getMetrics(dm);
         Constants.SCREEN_WIDTH = dm.widthPixels;
         Constants.SCREEN_HEIGHT = dm.heightPixels;
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
         txt_score = findViewById(R.id.txt_score);
-        txt_best_score = findViewById(R.id.txt_best_score);
+        txt_best_score = findViewById(R.id.highScore);
         txt_score_over = findViewById(R.id.txt_score_over);
         rl_game_over = findViewById(R.id.rl_game_over);
         txt_welcome = findViewById (R.id.txt_welcome);
